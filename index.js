42282912150f98c722e08e6e9386cf046908ce82
Ashington Munene <munenefrank11@gmail.com>
2025-06-09 14:51:33 +0300

    creates functions for else f, ternary ans switch

--- index.js
+++ index.js
@@ -1,11 +1,44 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(feet){
   // Write your code here!
-}
+  let result;
 
-function ternaryCheckCity(){
-  // Write your code here!
-}
+  if (feet <=400) {
+    result = `This one is on me!`;
+  }
+  else if (feet > 400 && feet <= 2000) {
+    result = 'That will be twenty bucks.';
+  }
+  else if (feet > 2000 && feet <= 2500) {
+    result = 'I will gladly take your thirty bucks.';
+  }
+  else if (feet > 2500) {
+   result = 'No can do.';
+  }
+  return result;
+  
+  }
 
-function switchOnCharmFromTip(){
+
+function ternaryCheckCity(city){
   // Write your code here!
+  return (city === 'NYC') ? 'Ok, sounds good.' : 'No go.';
 }
+  console.log(ternaryCheckCity("NYC"));         
+  console.log(ternaryCheckCity("Pittsburgh"));
+
+function switchOnCharmFromTip(tip){
+  // Write your code here!
+ switch (tip) {
+    case 'generous':
+      return "Thank you so much.";
+      break;
+    case `not as generous`:
+      return'Thank you.';
+      break
+    case `thanks for everything`:
+      return `Bye.`;
+      break;
+    default:
+      return 'Bye.'
+      }
+ }
